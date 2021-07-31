6e716be30f79184ea17274ba03eef04ef20bfb45
Ron Jensen <ron@jentronics.com>
2021-07-30 18:30:03 -0600

    Cleanup ApplyPMD.py

--- scripts/ApplyPMD.py
+++ scripts/ApplyPMD.py
@@ -1,7 +1,15 @@
-readmorph=bpy.data.texts['LIBReadPZMD.py'].as_module()
-pmdPath='' # full path to the PMD file
+
+import bpy
+from bpy import data as D
+from bpy import context as C
+from mathutils import *
+from math import *
+
+
+readmorph=bpy.data.texts['ReadPZMD.py'].as_module()
+pmdPath=r'' # full path to the PMD file
 morphs=readmorph.readPZMD(pmdPath)
 
-applymorph=bpy.data.texts['LIBApplyMorph.py'].as_module()
+applymorph=bpy.data.texts['ApplyMorph.py'].as_module()
 for morph in morphs:
      applymorph.ApplyMorph(D.objects['MeshObject'], morph)
